38e3e537b01e1d421470ced6cbb9b1dddbd6e117
Kofi Appiah-Nkansah <kofinkansah@gmail.com>
2015-10-21 17:40:48 +0000

    Created models for photos and setup associations

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,9 +1,10 @@
 class Place < ActiveRecord::Base
 	belongs_to :user
 	has_many :comments
+	has_many :photos
 	
 	geocoded_by :address
-  after_validation :geocode
+  	after_validation :geocode
 
 	validates :name, :presence => true, :uniqueness => true, :length => { :minimum => 3}
 	validates :address, :description, :presence => true
